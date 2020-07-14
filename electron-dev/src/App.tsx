78509908752841f6fbc9c3ace72d00462e645b3d
이동규 <dongkyu@MacBook-Pro.local>
2020-07-14 19:26:15 +0900

    Example of Electron-React UI

--- electron-dev/src/App.tsx
+++ electron-dev/src/App.tsx
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export interface Props {
+  name: String;
+  enthusiasmLevel?: number;
+}
+
+function App({ name, enthusiasmLevel = 1 }: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You could be a little more enthusiastic.');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="greeting">
+        This is Example of {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!');
+}
